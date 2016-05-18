7f3f5c0a808adda5c149ecf5f0805f765e0bb6fe
Continuous Integration <mark+ci@zontak.me>
2016-05-18 10:51:14 +0000

    CI commit

--- dist/pip-webui.js
+++ dist/pip-webui.js
@@ -26468,6 +26468,21 @@ module.run(['$templateCache', function($templateCache) {
 })();
 
 
+/**
+ * @file Registration of support all pages
+ * @copyright Digital Living Software Corp. 2014-2016
+ */
+
+/* global angular */
+
+(function () {
+    'use strict';
+
+    angular.module('pipSupport', [
+        'pipFeedback',
+        'pipAnalytics'
+    ]);
+})();
 (function(module) {
 try {
   module = angular.module('pipSupport.Templates');
@@ -26730,21 +26745,6 @@ module.run(['$templateCache', function($templateCache) {
 }]);
 })();
 
-/**
- * @file Registration of support all pages
- * @copyright Digital Living Software Corp. 2014-2016
- */
-
-/* global angular */
-
-(function () {
-    'use strict';
-
-    angular.module('pipSupport', [
-        'pipFeedback',
-        'pipAnalytics'
-    ]);
-})();
 /**
  * @file Web analytics service
  * @copyright Digital Living Software Corp. 2014-2016
