ae602e63c0e865e487297d47e976864637648717
Continuous Integration <mark+ci@zontak.me>
2016-05-30 13:42:46 +0000

    CI commit

--- dist/pip-webui.js
+++ dist/pip-webui.js
@@ -26175,7 +26175,7 @@ module.run(['$templateCache', function($templateCache) {
     var thisModule = angular.module('pipTips.Service', ['pipGuidance.Templates']);
 
     thisModule.factory('pipTips', ['$pipPopover', 'pipTipsData', 'pipRest', '$timeout', '$rootScope', 'pipSettingsData', function ($pipPopover, pipTipsData, pipRest, $timeout, $rootScope, pipSettingsData) {
-            var tips;
+            var tips, topic, settings;
             
             return {
                 getTips: getTips,
@@ -26188,16 +26188,25 @@ module.run(['$templateCache', function($templateCache) {
                 return item.status == 'completed' ? true : false;
             }
 
+            function checkExtendId(item) {
+                if(!settings[topic]) return true;
+                return !_.find(settings[topic].extends, {id: item.id});
+            }
+
             function compareRandom(a, b) {
                 return Math.random() - 0.5;
             }
 
-            function filterTips(data, topic) {
+            function filterTips(data, topicLocal, settingsLocal) {
                 tips = [];
+                settings = settingsLocal;
+                topic = topicLocal;
                 var tipsCollection = _.filter(data, checkStatus);
+                tipsCollection = _.filter(tipsCollection, checkExtendId);
+
                 for (var index = 0; index < tipsCollection.length; index++) {
-                    var topic = _.find(tipsCollection[index].topics, function (t) { return t == topic; });                    
-                    if (topic) {
+                    var topicLocal = _.find(tipsCollection[index].topics, function (t) { return t == topicLocal; });
+                    if (topicLocal) {
                         tips.push(tipsCollection[index]);
                     }
                 }
@@ -26215,6 +26224,24 @@ module.run(['$templateCache', function($templateCache) {
                 init();
 
                 $scope.onNextClick = function () {
+                    console.log($scope.locals.settings, $scope.locals.topic);
+                    if($scope.locals.settings && $scope.locals.topic && $scope.locals.settings[$scope.locals.topic]){
+                        if(!$scope.locals.settings[$scope.locals.topic].extends) {
+                            $scope.locals.settings[$scope.locals.topic].extends = [];
+                        }
+                        $scope.locals.settings[$scope.locals.topic].extends.push($scope.locals.tips[$scope.index].id);
+
+                        var eIds = $scope.locals.settings[$scope.locals.topic].extends;
+
+                        if($scope.locals.settings[$scope.locals.topic].extends.length > 10){
+                            for( var i = 0; i < 10; i++){
+                                $scope.locals.settings[$scope.locals.topic].extends[i] = eIds[$scope.eIds.length -10 + i];
+                            }
+                        }
+                        console.log($scope.locals.settings);
+                        pipSettingsData.saveSettings($scope.locals.settings, $scope.locals.topic);
+                    }
+
                     $scope.index++;
                     if ($scope.index == $scope.locals.tips.length)
                         $pipPopover.hide();
@@ -26263,7 +26290,8 @@ module.run(['$templateCache', function($templateCache) {
                         },
                         locals: {
                             tips: tips,
-                            ln: ln || 'en'
+                            ln: ln || 'en',
+                            settings: settings
                         },
                         controller: ['$scope', '$timeout', '$mdMedia', tipController],
                         templateUrl: 'tips/tip.template.html'
