72a36aa2b2864babc8313ecb571013090e84c361
Continuous Integration <mark+ci@zontak.me>
2016-05-30 17:40:15 +0000

    CI commit

--- dist/pip-webui.js
+++ dist/pip-webui.js
@@ -25721,24 +25721,6 @@ module.run(['$templateCache', function($templateCache) {
 
 
 
-/**
- * @file Registration of all guidance components
- * @copyright Digital Living Software Corp. 2014-2016
- */
-
-/* global angular */
-
-(function () {
-    'use strict';
-
-    angular.module('pipGuidance', [
-        'pipTips.Service',
-        'pipIntroGuidance.Service',
-        'pipGuidance.Dialog',
-        'pipReleaseIntroDialog'
-    ]);
-    
-})();
 (function(module) {
 try {
   module = angular.module('pipGuidance.Templates');
@@ -25903,6 +25885,24 @@ module.run(['$templateCache', function($templateCache) {
 }]);
 })();
 
+/**
+ * @file Registration of all guidance components
+ * @copyright Digital Living Software Corp. 2014-2016
+ */
+
+/* global angular */
+
+(function () {
+    'use strict';
+
+    angular.module('pipGuidance', [
+        'pipTips.Service',
+        'pipIntroGuidance.Service',
+        'pipGuidance.Dialog',
+        'pipReleaseIntroDialog'
+    ]);
+    
+})();
 /**
  * @file Guidance dialog
  * @copyright Digital Living Software Corp. 2014-2015
@@ -26175,8 +26175,8 @@ module.run(['$templateCache', function($templateCache) {
     var thisModule = angular.module('pipTips.Service', ['pipGuidance.Templates']);
 
     thisModule.factory('pipTips', ['$pipPopover', 'pipTipsData', 'pipRest', '$timeout', '$rootScope', 'pipSettingsData', function ($pipPopover, pipTipsData, pipRest, $timeout, $rootScope, pipSettingsData) {
-            var tips, topic, settings;
-            
+            var tips;
+
             return {
                 getTips: getTips,
                 filterTips: filterTips,
@@ -26188,25 +26188,16 @@ module.run(['$templateCache', function($templateCache) {
                 return item.status == 'completed' ? true : false;
             }
 
-            function checkExtendId(item) {
-                if(!settings[topic]) return true;
-                return !_.find(settings[topic].extends, {id: item.id});
-            }
-
             function compareRandom(a, b) {
                 return Math.random() - 0.5;
             }
 
-            function filterTips(data, topicLocal, settingsLocal) {
+            function filterTips(data, topic) {
                 tips = [];
-                settings = settingsLocal;
-                topic = topicLocal;
                 var tipsCollection = _.filter(data, checkStatus);
-                tipsCollection = _.filter(tipsCollection, checkExtendId);
-
                 for (var index = 0; index < tipsCollection.length; index++) {
-                    var topicLocal = _.find(tipsCollection[index].topics, function (t) { return t == topicLocal; });
-                    if (topicLocal) {
+                    var topic = _.find(tipsCollection[index].topics, function (t) { return t == topic; });
+                    if (topic) {
                         tips.push(tipsCollection[index]);
                     }
                 }
@@ -26214,7 +26205,7 @@ module.run(['$templateCache', function($templateCache) {
                 return tips;
 
             }
-            
+
             function tipController($scope, $timeout, $mdMedia) {
 
                 $scope.index = 0;
@@ -26224,24 +26215,6 @@ module.run(['$templateCache', function($templateCache) {
                 init();
 
                 $scope.onNextClick = function () {
-                    console.log($scope.locals.settings, $scope.locals.topic);
-                    if($scope.locals.settings && $scope.locals.topic && $scope.locals.settings[$scope.locals.topic]){
-                        if(!$scope.locals.settings[$scope.locals.topic].extends) {
-                            $scope.locals.settings[$scope.locals.topic].extends = [];
-                        }
-                        $scope.locals.settings[$scope.locals.topic].extends.push($scope.locals.tips[$scope.index].id);
-
-                        var eIds = $scope.locals.settings[$scope.locals.topic].extends;
-
-                        if($scope.locals.settings[$scope.locals.topic].extends.length > 10){
-                            for( var i = 0; i < 10; i++){
-                                $scope.locals.settings[$scope.locals.topic].extends[i] = eIds[$scope.eIds.length -10 + i];
-                            }
-                        }
-                        console.log($scope.locals.settings);
-                        pipSettingsData.saveSettings($scope.locals.settings, $scope.locals.topic);
-                    }
-
                     $scope.index++;
                     if ($scope.index == $scope.locals.tips.length)
                         $pipPopover.hide();
@@ -26260,7 +26233,7 @@ module.run(['$templateCache', function($templateCache) {
                     $scope.title = $scope.locals.tips[$scope.index].title[$scope.locals.ln];
                     $scope.content = $scope.locals.tips[$scope.index].content[$scope.locals.ln];
                     if ($scope.locals.tips[$scope.index].pic_id) {
-                        $scope.image = pipRest.serverUrl() + '/api/parties/' + $scope.locals.tips[$scope.index].creator_id 
+                        $scope.image = pipRest.serverUrl() + '/api/parties/' + $scope.locals.tips[$scope.index].creator_id
                             + '/files/' + $scope.locals.tips[$scope.index].pic_id + '/content';
                     }
 
@@ -26290,8 +26263,7 @@ module.run(['$templateCache', function($templateCache) {
                         },
                         locals: {
                             tips: tips,
-                            ln: ln || 'en',
-                            settings: settings
+                            ln: ln || 'en'
                         },
                         controller: ['$scope', '$timeout', '$mdMedia', tipController],
                         templateUrl: 'tips/tip.template.html'
@@ -26300,7 +26272,7 @@ module.run(['$templateCache', function($templateCache) {
 
 
             }
-          
+
             function firstShowTips(tips, ln, topic, settings, kolDay) {
                 var ln = ln || 'en';
                 var kolDay = kolDay || 2;
