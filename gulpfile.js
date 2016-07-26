06671ffd699425dc56067b7cdac138f165217f2b
Sergey Seroukhov <seroukhov@gmail.com>
2016-07-25 20:37:40 -0700

    Changed references

--- gulpfile.js
+++ gulpfile.js
@@ -16,21 +16,21 @@ var
 
 gulp.task('build-js-dev', function() {
     return gulp.src([
-            './node_modules/pip-webui-core/dist/pip-webui-core.js',
-            './node_modules/pip-webui-rest/dist/pip-webui-rest.js',
-            './node_modules/pip-webui-layouts/dist/pip-webui-layouts.js',
-            './node_modules/pip-webui-controls/dist/pip-webui-controls.js',
-            './node_modules/pip-webui-nav/dist/pip-webui-nav.js',
-            './node_modules/pip-webui-locations/dist/pip-webui-locations.js',
-            './node_modules/pip-webui-documents/dist/pip-webui-documents.js',
-            './node_modules/pip-webui-pictures/dist/pip-webui-pictures.js',
-            './node_modules/pip-webui-composite/dist/pip-webui-composite.js',
-            './node_modules/pip-webui-entry/dist/pip-webui-entry.js',
-            './node_modules/pip-webui-errors/dist/pip-webui-errors.js',
-            './node_modules/pip-webui-settings/dist/pip-webui-settings.js',
-            './node_modules/pip-webui-guidance/dist/pip-webui-guidance.js',
-            './node_modules/pip-webui-help/dist/pip-webui-help.js',
-            './node_modules/pip-webui-support/dist/pip-webui-support.js'
+            '../pip-webui-core/dist/pip-webui-core.js',
+            '../pip-webui-rest/dist/pip-webui-rest.js',
+            '../pip-webui-layouts/dist/pip-webui-layouts.js',
+            '../pip-webui-controls/dist/pip-webui-controls.js',
+            '../pip-webui-nav/dist/pip-webui-nav.js',
+            '../pip-webui-locations/dist/pip-webui-locations.js',
+            '../pip-webui-documents/dist/pip-webui-documents.js',
+            '../pip-webui-pictures/dist/pip-webui-pictures.js',
+            '../pip-webui-composite/dist/pip-webui-composite.js',
+            '../pip-webui-entry/dist/pip-webui-entry.js',
+            '../pip-webui-errors/dist/pip-webui-errors.js',
+            '../pip-webui-settings/dist/pip-webui-settings.js',
+            '../pip-webui-guidance/dist/pip-webui-guidance.js',
+            '../pip-webui-help/dist/pip-webui-help.js',
+            '../pip-webui-support/dist/pip-webui-support.js'
         ])
         .pipe(sourceMaps.init({ loadMaps: true }))
         .pipe(concat(pkg.name + '.js'))
@@ -40,21 +40,21 @@ gulp.task('build-js-dev', function() {
 
 gulp.task('build-js-prod', function() {
     return gulp.src([
-            './node_modules/pip-webui-core/dist/pip-webui-core.min.js',
-            './node_modules/pip-webui-rest/dist/pip-webui-rest.min.js',
-            './node_modules/pip-webui-layouts/dist/pip-webui-layouts.min.js',
-            './node_modules/pip-webui-controls/dist/pip-webui-controls.min.js',
-            './node_modules/pip-webui-nav/dist/pip-webui-nav.min.js',
-            './node_modules/pip-webui-locations/dist/pip-webui-locations.min.js',
-            './node_modules/pip-webui-documents/dist/pip-webui-documents.min.js',
-            './node_modules/pip-webui-pictures/dist/pip-webui-pictures.min.js',
-            './node_modules/pip-webui-composite/dist/pip-webui-composite.min.js',
-            './node_modules/pip-webui-entry/dist/pip-webui-entry.min.js',
-            './node_modules/pip-webui-errors/dist/pip-webui-errors.min.js',
-            './node_modules/pip-webui-settings/dist/pip-webui-settings.min.js',
-            './node_modules/pip-webui-guidance/dist/pip-webui-guidance.min.js',
-            './node_modules/pip-webui-help/dist/pip-webui-help.min.js',
-            './node_modules/pip-webui-support/dist/pip-webui-support.min.js'
+            '../pip-webui-core/dist/pip-webui-core.min.js',
+            '../pip-webui-rest/dist/pip-webui-rest.min.js',
+            '../pip-webui-layouts/dist/pip-webui-layouts.min.js',
+            '../pip-webui-controls/dist/pip-webui-controls.min.js',
+            '../pip-webui-nav/dist/pip-webui-nav.min.js',
+            '../pip-webui-locations/dist/pip-webui-locations.min.js',
+            '../pip-webui-documents/dist/pip-webui-documents.min.js',
+            '../pip-webui-pictures/dist/pip-webui-pictures.min.js',
+            '../pip-webui-composite/dist/pip-webui-composite.min.js',
+            '../pip-webui-entry/dist/pip-webui-entry.min.js',
+            '../pip-webui-errors/dist/pip-webui-errors.min.js',
+            '../pip-webui-settings/dist/pip-webui-settings.min.js',
+            '../pip-webui-guidance/dist/pip-webui-guidance.min.js',
+            '../pip-webui-help/dist/pip-webui-help.min.js',
+            '../pip-webui-support/dist/pip-webui-support.min.js'
         ])
         //.pipe(sourceMaps.init({ loadMaps: true }))
         .pipe(concat(pkg.name + '.min.js'))
@@ -65,20 +65,20 @@ gulp.task('build-js-prod', function() {
 
 gulp.task('build-css-dev', function() {
     return gulp.src([
-            './node_modules/pip-webui-css/dist/pip-webui-css.css',
-            './node_modules/pip-webui-layouts/dist/pip-webui-layouts.css',
-            './node_modules/pip-webui-controls/dist/pip-webui-controls.css',
-            './node_modules/pip-webui-nav/dist/pip-webui-nav.css',
-            './node_modules/pip-webui-locations/dist/pip-webui-locations.css',
-            './node_modules/pip-webui-documents/dist/pip-webui-documents.css',
-            './node_modules/pip-webui-pictures/dist/pip-webui-pictures.css',
-            './node_modules/pip-webui-composite/dist/pip-webui-composite.css',
-            './node_modules/pip-webui-entry/dist/pip-webui-entry.css',
-            './node_modules/pip-webui-errors/dist/pip-webui-errors.css',
-            './node_modules/pip-webui-settings/dist/pip-webui-settings.css',
-            './node_modules/pip-webui-guidance/dist/pip-webui-guidance.css',
-            './node_modules/pip-webui-help/dist/pip-webui-help.css',
-            './node_modules/pip-webui-support/dist/pip-webui-support.css'
+            '../pip-webui-css/dist/pip-webui-css.css',
+            '../pip-webui-layouts/dist/pip-webui-layouts.css',
+            '../pip-webui-controls/dist/pip-webui-controls.css',
+            '../pip-webui-nav/dist/pip-webui-nav.css',
+            '../pip-webui-locations/dist/pip-webui-locations.css',
+            '../pip-webui-documents/dist/pip-webui-documents.css',
+            '../pip-webui-pictures/dist/pip-webui-pictures.css',
+            '../pip-webui-composite/dist/pip-webui-composite.css',
+            '../pip-webui-entry/dist/pip-webui-entry.css',
+            '../pip-webui-errors/dist/pip-webui-errors.css',
+            '../pip-webui-settings/dist/pip-webui-settings.css',
+            '../pip-webui-guidance/dist/pip-webui-guidance.css',
+            '../pip-webui-help/dist/pip-webui-help.css',
+            '../pip-webui-support/dist/pip-webui-support.css'
         ])
         .pipe(concat(pkg.name + '.css'))
         .pipe(gulp.dest('./dist')); 
@@ -86,20 +86,20 @@ gulp.task('build-css-dev', function() {
 
 gulp.task('build-css-prod', function() {
     return gulp.src([
-            './node_modules/pip-webui-css/dist/pip-webui-css.min.css',
-            './node_modules/pip-webui-layouts/dist/pip-webui-layouts.min.css',
-            './node_modules/pip-webui-controls/dist/pip-webui-controls.min.css',
-            './node_modules/pip-webui-nav/dist/pip-webui-nav.min.css',
-            './node_modules/pip-webui-locations/dist/pip-webui-locations.min.css',
-            './node_modules/pip-webui-documents/dist/pip-webui-documents.min.css',
-            './node_modules/pip-webui-pictures/dist/pip-webui-pictures.min.css',
-            './node_modules/pip-webui-composite/dist/pip-webui-composite.min.css',
-            './node_modules/pip-webui-entry/dist/pip-webui-entry.min.css',
-            './node_modules/pip-webui-errors/dist/pip-webui-errors.min.css',
-            './node_modules/pip-webui-settings/dist/pip-webui-settings.min.css',
-            './node_modules/pip-webui-guidance/dist/pip-webui-guidance.min.css',
-            './node_modules/pip-webui-help/dist/pip-webui-help.min.css',
-            './node_modules/pip-webui-support/dist/pip-webui-support.min.css'
+            '../pip-webui-css/dist/pip-webui-css.min.css',
+            '../pip-webui-layouts/dist/pip-webui-layouts.min.css',
+            '../pip-webui-controls/dist/pip-webui-controls.min.css',
+            '../pip-webui-nav/dist/pip-webui-nav.min.css',
+            '../pip-webui-locations/dist/pip-webui-locations.min.css',
+            '../pip-webui-documents/dist/pip-webui-documents.min.css',
+            '../pip-webui-pictures/dist/pip-webui-pictures.min.css',
+            '../pip-webui-composite/dist/pip-webui-composite.min.css',
+            '../pip-webui-entry/dist/pip-webui-entry.min.css',
+            '../pip-webui-errors/dist/pip-webui-errors.min.css',
+            '../pip-webui-settings/dist/pip-webui-settings.min.css',
+            '../pip-webui-guidance/dist/pip-webui-guidance.min.css',
+            '../pip-webui-help/dist/pip-webui-help.min.css',
+            '../pip-webui-support/dist/pip-webui-support.min.css'
         ])
         .pipe(concat(pkg.name + '.min.css'))
         .pipe(gulp.dest('./dist')); 
@@ -107,7 +107,7 @@ gulp.task('build-css-prod', function() {
 
 gulp.task('build-less', function() {
     return gulp.src([
-            './node_modules/pip-webui-css/dist/pip-webui-css.less'
+            '../pip-webui-css/dist/pip-webui-css.less'
         ])
         .pipe(concat(pkg.name + '.less'))
         .pipe(gulp.dest('./dist'));
@@ -118,26 +118,26 @@ gulp.task('build-prod', ['build-js-prod', 'build-css-prod']);
 
 gulp.task('copy-images', function() {
     return gulp.src([
-            './node_modules/pip-webui-controls/dist/images/*',
-            './node_modules/pip-webui-nav/dist/images/*',
-            './node_modules/pip-webui-locations/dist/images/*',
-            './node_modules/pip-webui-documents/dist/images/*',
-            './node_modules/pip-webui-pictures/dist/images/*',
-            './node_modules/pip-webui-composite/dist/images/*',
-            './node_modules/pip-webui-entry/dist/images/*',
-            './node_modules/pip-webui-errors/dist/images/*',
-            './node_modules/pip-webui-settings/dist/images/*',
-            './node_modules/pip-webui-guidance/dist/images/*',
-            './node_modules/pip-webui-help/dist/images/*',
-            './node_modules/pip-webui-support/dist/images/*',
-            './node_modules/pip-webui-css/dist/images/*'
+            '../pip-webui-controls/dist/images/*',
+            '../pip-webui-nav/dist/images/*',
+            '../pip-webui-locations/dist/images/*',
+            '../pip-webui-documents/dist/images/*',
+            '../pip-webui-pictures/dist/images/*',
+            '../pip-webui-composite/dist/images/*',
+            '../pip-webui-entry/dist/images/*',
+            '../pip-webui-errors/dist/images/*',
+            '../pip-webui-settings/dist/images/*',
+            '../pip-webui-guidance/dist/images/*',
+            '../pip-webui-help/dist/images/*',
+            '../pip-webui-support/dist/images/*',
+            '../pip-webui-css/dist/images/*'
         ])
         .pipe(gulp.dest('./dist/images')); 
 });
 
 gulp.task('copy-lib', function() {
     return gulp.src([
-            './node_modules/pip-webui-lib/dist/**/*'
+            '../pip-webui-lib/dist/**/*'
         ])
         .pipe(gulp.dest('./dist')); 
 });
