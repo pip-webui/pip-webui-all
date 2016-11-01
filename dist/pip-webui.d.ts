8f8b6f729670d1c73b770e2b6c9dfafb956b8a84
Sergey Seroukhov <seroukhov@gmail.com>
2016-11-01 12:03:26 -0500

    Rebuilt

--- dist/pip-webui.d.ts
+++ dist/pip-webui.d.ts
@@ -10,9 +10,14 @@ declare module pip.services {
 
 
 
-
 export let CurrentState: any;
 export let PreviousState: any;
+
+
+
+
+
+
 
 let RedirectedStates: any;
 function decorateRedirectStateProvider($delegate: any): any;
@@ -21,10 +26,6 @@ function decorateRedirectStateService($delegate: any, $timeout: any): any;
 function addRedirectStateDecorator($provide: any): void;
 
 export let RoutingVar: string;
-
-
-
-
 
 export let IdentityRootVar: string;
 export let IdentityChangedEvent: string;
@@ -46,6 +47,8 @@ export interface IIdentityProvider extends ng.IServiceProvider {
 
 
 
+
+
 export const SessionRootVar: string;
 export const SessionOpenedEvent: string;
 export const SessionClosedEvent: string;
@@ -59,6 +62,14 @@ export interface ISessionProvider extends ng.IServiceProvider {
     setRootVar: boolean;
     session: any;
 }
+
+
+
+
+
+
+
+
 
 
 export class Transaction {
@@ -97,9 +108,6 @@ export class TransactionError {
 }
 
 
-
-
-
 export interface ITransactionService {
     create(scope?: string): Transaction;
     get(scope?: string): Transaction;
@@ -107,15 +115,18 @@ export interface ITransactionService {
 
 function configureTransactionStrings($injector: any): void;
 
+
+
+
+
+
+
+
 function translateDirective(pipTranslate: any): ng.IDirective;
 function translateHtmlDirective(pipTranslate: any): ng.IDirective;
 
 function translateFilter(pipTranslate: any): (key: any) => any;
 function optionalTranslateFilter($injector: any): (key: any) => any;
-
-
-
-
 
 export let LanguageRootVar: string;
 export let LanguageChangedEvent: string;
@@ -178,6 +189,21 @@ export interface IFormat {
     sample(value: string, maxLength: number): string;
     sprintf(message: string, ...args: any[]): string;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export let ResetPageEvent: string;
 export let ResetAreaEvent: string;
@@ -248,7 +274,41 @@ declare module pip.layouts {
 
 
 
-var Masonry: any;
+export class MediaBreakpoints {
+    constructor(xs: number, sm: number, md: number, lg: number);
+    xs: number;
+    sm: number;
+    md: number;
+    lg: number;
+}
+export class MediaBreakpointStatuses {
+    width: number;
+    'xs': boolean;
+    'gt-xs': boolean;
+    'sm': boolean;
+    'gt-sm': boolean;
+    'md': boolean;
+    'gt-md': boolean;
+    'lg': boolean;
+    'gt-lg': boolean;
+    'xl': boolean;
+    update(breakpoints: MediaBreakpoints, width: number): void;
+}
+export let MainResizedEvent: string;
+export let LayoutResizedEvent: string;
+export let MainBreakpoints: MediaBreakpoints;
+export let MainBreakpointStatuses: MediaBreakpointStatuses;
+export interface IMediaService {
+    (breakpoint: string): boolean;
+    breakpoints: MediaBreakpoints;
+    width: number;
+}
+export interface IMediaProvider extends ng.IServiceProvider {
+    breakpoints: MediaBreakpoints;
+}
+
+export function addResizeListener(element: any, listener: any): void;
+export function removeResizeListener(element: any, listener: any): void;
 
 }
 
@@ -619,6 +679,45 @@ declare module pip.themes {
 
 
 
+function configureBootBarnCoolTheme($mdThemingProvider: ng.material.IThemingProvider): void;
+
+function configureBootBarnMonochromeTheme($mdThemingProvider: ng.material.IThemingProvider): void;
+
+function configureBootBarnWarmTheme($mdThemingProvider: any): void;
+
+
+
+
+
+
+
+
+export let ThemeRootVar: string;
+export let ThemeChangedEvent: string;
+export interface IThemeService {
+    theme: string;
+    use(language: string): string;
+}
+export interface IThemeProvider extends IThemeService, ng.IServiceProvider {
+    setRootVar: boolean;
+    persist: boolean;
+}
+
+function configureDefaultAmberTheme($mdThemingProvider: ng.material.IThemingProvider): void;
+
+function configureDefaultBlackTheme($mdThemingProvider: ng.material.IThemingProvider): void;
+
+function configureDefaultBlueTheme($mdThemingProvider: ng.material.IThemingProvider): void;
+
+function configureDefaultGreenTheme($mdThemingProvider: ng.material.IThemingProvider): void;
+
+function configureDefaultGreyTheme($mdThemingProvider: ng.material.IThemingProvider): void;
+
+function configureDefaultNavyTheme($mdThemingProvider: ng.material.IThemingProvider): void;
+
+function configureDefaultOrangeTheme($mdThemingProvider: ng.material.IThemingProvider): void;
+
+function configureDefaultPinkTheme($mdThemingProvider: ng.material.IThemingProvider): void;
 
 
 
